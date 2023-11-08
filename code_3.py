12a8b17683653a8b75aa42213b3be5283bf085af
Jack <143327277+Jack30046@users.noreply.github.com>
2023-11-08 07:27:53 +0000

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,54 @@
+public class RandomDataGenerator {
+        }
+        }
+            System.out.println("Random Number: " + item);
+    }
+            data[i] = (int) (Math.random() * 100) + 1;
+}
+    public static void main(String[] args) {
+
+        for (int item : data) {
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+    return data;
+        data.push(Math.floor(Math.random() * 100) + 1);
+main();
+}
+function main() {
+
+    data.forEach(item => {
+    }
+    });
+    for (let i = 0; i < 10; i++) {
+    const data = [];
+        console.log("Random Number: " + item);
+}
+
+
+function generateRandomData() {
+    const data = generateRandomData();if __name__ == "__main__":
+def generate_random_data():
+    data = generate_random_data()
+    for item in data:
+    main()
+        print(f"Random Number: {item}")
+
+import random
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+
+
+def main():def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+    for item in data:
+        print(f"Random Number: {item}")
+
+import random
+    data = generate_random_data()
+
+if __name__ == "__main__":
+
+def generate_random_data():
+    return data
